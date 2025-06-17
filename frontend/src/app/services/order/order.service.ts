d9ba4bb5574429f34ce00c8190a6fca2ee7e6e58
Missaoui Raslen <149624976+raslenraslen@users.noreply.github.com>
2025-06-18 01:10:39 +0200

    Update order.service.ts

--- frontend/src/app/services/order/order.service.ts
+++ frontend/src/app/services/order/order.service.ts
@@ -19,6 +19,6 @@ export class OrderService {
       }),
       responseType: 'text' as 'json'
     };
-    return this.httpClient.post<string>('http://localhost:9000/api/order', order, httpOptions);
+    return this.httpClient.post<string>('http://order-service.cloudforge.svc.cluster.local:8081/api/order', order, httpOptions);
   }
 }
