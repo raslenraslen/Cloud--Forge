8e4ffcf985f76432eec622d119c4eeb336c9218e
Missaoui Raslen <149624976+raslenraslen@users.noreply.github.com>
2025-06-18 01:09:19 +0200

    Update product.service.ts

--- frontend/src/app/services/product/product.service.ts
+++ frontend/src/app/services/product/product.service.ts
@@ -12,10 +12,10 @@ export class ProductService {
   }
 
   getProducts(): Observable<Array<Product>> {
-    return this.httpClient.get<Array<Product>>('http://localhost:9000/api/product');
+    return this.httpClient.get<Array<Product>>('http://product-service.cloudforge.svc.cluster.local:8080/api/product');
   }
 
   createProduct(product: Product): Observable<Product> {
-    return this.httpClient.post<Product>('http://localhost:9000/api/product', product);
+    return this.httpClient.post<Product>('http://product-service.cloudforge.svc.cluster.local:8080/api/product', product);
   }
 }
